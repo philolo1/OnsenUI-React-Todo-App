2b96cdd637d8ed40ec2daf9e37fbfe8c93ae503a
Patrick Klitzke <philologos14@gmail.com>
2016-05-16 17:15:15 +0900

    add cordova project

---
+++ todo-app/src/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Page, Button} from 'react-onsenui';
+
+// load Onsen UI library
+import 'onsenui';
+import App from './App.js';
+
+ReactDOM.render(<App />, document.getElementById('app'));
